926c7d6b86f6727f52069b91d9419ad007b87290
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-09-25 02:07:26 +0700

    [UPDATE] Menyelesaikan exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,24 +1,26 @@
-const SeleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+const valueSelection = (firstValue, lastValue, dataArray) => {
+  // Validate input
   if (dataArray.length <= 5) {
     return "Jumlah angka dalam dataArray harus lebih dari 5";
   }
-
-  if (nilaiAwal >= nilaiAkhir) {
+  if (firstValue >= lastValue) {
     return "Nilai akhir harus lebih besar dari nilai awal";
   }
 
-  const hasilPencarian = dataArray.filter((item) => item > nilaiAwal && item < nilaiAkhir);
+  // Search for data in the array
+  const searchResult = dataArray.filter((item) => item > firstValue && item < lastValue);
 
-  if (hasilPencarian.length === 0) {
+  if (searchResult.length === 0) {
     return "Nilai tidak ditemukan";
   }
+  // Sort search results
+  searchResult.sort((a, b) => a - b);
 
-  hasilPencarian.sort((a, b) => a - b);
-
-  return hasilPencarian;
+  return searchResult;
 };
 
-console.log(SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])); // Output: [8, 14, 17]
-console.log(SeleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8])); // Output: "Nilai akhir harus lebih besar dari nilai awal"
-console.log(SeleksiNilai(5, 17, [2, 25, 4])); // Output: "Jumlah angka dalam dataArray harus lebih dari 5"
-console.log(SeleksiNilai(5, 17, [2, 25, 4, 1, 30, 18])); // Output: "Nilai tidak ditemukan"
+// Execute
+console.log(valueSelection(5, 20, [2, 25, 4, 14, 17, 30, 8])); // Output: [8, 14, 17]
+console.log(valueSelection(15, 3, [2, 25, 4, 14, 17, 30, 8])); // Output: "Nilai akhir harus lebih besar dari nilai awal"
+console.log(valueSelection(5, 17, [2, 25, 4])); // Output: "Jumlah angka dalam dataArray harus lebih dari 5"
+console.log(valueSelection(5, 17, [2, 25, 4, 1, 30, 18])); // Output: "Nilai tidak ditemukan"
