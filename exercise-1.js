651738577490725fdd84e445163a6cc9ec089a86
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-09-25 07:23:52 +0700

    [UPDATE] Menyelesaikan exercise-1.js

--- exercise-1.js
+++ exercise-1.js
@@ -1,57 +1,69 @@
-// CONCAT() = untuk menggabungkan dua array atau lebih menjadi satu array
-const numConcat = [1, 2, 3];
-const strConcat = ["hai", "halo", "hai"];
-const resultConcat = numConcat.concat(strConcat);
-console.log("Result Concat = " + resultConcat);
+// 1. CONCAT () = The method functions to combine two or more arrays into one new array.
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = [7, 8, 9];
+const newArray = array1.concat(array2, array3);
 
-// INCLUDES() = untuk mengecek apakah sebuah elemen yang telah ditentukan terdapat pada array yang dimiliki
-const numIncludes = [1, 2, 3];
-const resultIncludes = numIncludes.includes(28);
-console.log("Result Includes = " + resultIncludes);
+console.log(newArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// SLICE() = untuk membagi elemen terpilih menjadi sebuah susunan objek baru.
-const numSlice = [1, 2, 3, 4, 5];
-const resultSlice = numSlice.slice(1, 4);
-console.log("Result Slice = " + resultSlice);
+// 2. INCLUDES() = This method functions to check whether data is in the array or not.
+const numbers = [1, 2, 3, 4, 5];
+const hasNumber3 = numbers.includes(3);
 
-// 07. pop() : untuk menghapus elemen terakhir dari array dan mengembalikkan element tersebut
-const hobbies = ["swimming", "reading", "singing", "dancing"];
-const last = hobbies.pop();
-console.log(last);
+console.log(hasNumber3); // Output: true
 
-// COPYWITHIN() = untuk menyalin sebuah value dari array ke posisi lain dalam array dengan menimpa value yang ada
-const numCopyWithin = [1, 2, 3, 4, 5];
-const resultCopyWithin = numCopyWithin.copyWithin(1, 3);
-console.log("Result Copywithin = " + resultCopyWithin);
+// 3. SLICE() = Method that functions to copy some array elements to a new array. The elements to copy are specified using index, start and end index.
+const fruits = ["apel", "mangga", "pisang", "ceri", "strawberi"];
+const slicedFruits = fruits.slice(1, 4);
 
-// FILL() = untuk mengubah sebuah value pada array dengan posisi yang spesifik
-const numFill = [1, 2, 3, 4, 5];
-const resultFill = numFill.fill(28, 1, 4);
-console.log("Result Fill = " + resultFill);
+console.log(slicedFruits); // Output: [ 'mangga', 'pisang', 'ceri' ]
 
-// POP() = untuk removes elemen terakhir dari sebuah array lalu mengembalikan elemen tersebut
-const numPop = [1, 2, 3, 4, 5];
-const resultPop = numPop.pop();
-console.log("Result Pop = " + resultPop);
+// 4. POP() = Method which functions to delete the last element of the array. This method changes the length of the array and returns the deleted elements.
+const fruits = ["apel", "mangga", "pisang", "ceri"];
+const removedFruit = fruits.pop();
 
-// SHIFT() = untuk menghapus elemen pertama dari sebuah array lalu mengembalikan elemen tersebut
-const numShift = [1, 2, 3, 4, 5];
-const resultShift = numShift.shift();
-console.log("Sesult Shift = " + resultShift);
+console.log(removedFruit); // Output: "ceri"
+console.log(fruits); // Output: [ 'apel', 'mangga', 'pisang' ]
 
-// UNSHIFT() = untuk menambahkan elemen baru pada index pertama array lalu mengembalikan nilai panjang array yang baru
-const numUnShift = [1, 2, 3, 4, 5];
-const resultUnShift = numUnShift.unshift(10, 11);
-console.log("Result Unshift = " + resultUnShift);
+// 5. COPYWITHIN() = Used to copy elements in an array and place them to another position in the array without changing their length.
+const numbers = [1, 2, 3, 4, 5];
+numbers.copyWithin(3, 0);
 
-// ENTRIES() = untuk menampilkan pasangan value array dengan keys nya
-const strEntries = ["a", "b", "c"];
-const entries = strEntries.entries();
-for (resultEntries of entries) {
-  console.log("Result Entries = " + resultEntries);
+console.log(numbers); // Output: [1, 2, 3, 1, 2]
+
+// 6. FILL() = Used to replace all elements in an array with the same value. This modifies the array itself and does not return a new array.
+const numbers = [1, 2, 3, 4, 5];
+numbers.fill(0);
+
+console.log(numbers); // Output: [0, 0, 0, 0, 0]
+
+// 7. POP() = Used to remove the last element from an array and return that element.
+const fruits = ["apel", "mangga", "pisang", "ceri"];
+const removedFruit = fruits.pop();
+
+console.log(removedFruit); // Output: "ceri"
+console.log(fruits); // Output: [ 'apel', 'mangga', 'pisang' ]
+
+// 8. SHIFT() = used to delete the first element of an array and shift the remaining elements to a lower position (lower index).
+const fruits = ["apel", "mangga", "pisang", "ceri"];
+const removedFruit = fruits.shift();
+
+console.log(removedFruit); // Output: "apel"
+console.log(fruits); // Output: [ 'mangga', 'pisang', 'ceri' ]
+
+// 9. UNSHIFT() = Used to add one or more new elements to the beginning of an array and shift the existing elements to a higher position (higher index).
+const fruits = ["mangga", "pisang", "ceri"];
+fruits.unshift("apel");
+
+console.log(fruits); // Output: [ 'apel', 'mangga', 'pisang', 'ceri' ]
+
+// ENTRIES() = used to create an iterator that contains index-value (key-value) pairs for each element in an array. This allows you to access the index and value of each element in the array.
+const fruits = ["apel", "mangga", "pisang"];
+const iterator = fruits.entries();
+for (const [index, value] of iterator) {
+  console.log(`Indeks: ${index}, Nilai: ${value}`);
 }
-
-// FIND() = mengembalikan nilai array yang memenuhi kondisi
-const numFind = [1, 2, 3, 4, 5, 48];
-const resultFind = numFind.find((num) => num > 30);
-console.log("Result Find = " + resultFind);
+// Output:
+// Indeks: 0, Nilai: apel
+// Indeks: 1, Nilai: mangga
+// Indeks: 2, Nilai: pisang
